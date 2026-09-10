c22d42603a3d22e69b579b2e15406646a8f68380
netfloww <wreosh3@gmail.com>
2026-09-11 02:36:26 +0300

    Create books1.js

---
+++ books1.js
@@ -0,0 +1,6 @@
+if (!window.booksData) window.booksData = {};
+window.booksData[1] = {
+    title: "العادات الذرية",
+    category: "تنمية بشرية",
+    desc: "طريقة سهلة ومثبتة لبناء العادات الجيدة وكسر العادات السيئة."
+};
