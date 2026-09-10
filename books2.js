fa34b51c4ebd006b4e9746e31d410a7c234ff744
netfloww <wreosh3@gmail.com>
2026-09-11 02:42:11 +0300

    Create books2.js

---
+++ books2.js
@@ -0,0 +1,6 @@
+if (!window.booksData) window.booksData = {};
+window.booksData[2] = {
+    title: "الأب الغني والأب الفقير",
+    category: "المال والأعمال",
+    desc: "ما يخبئه الأغنياء لأبنائهم ولا يفعله الفقراء والطبقة الوسطى."
+};
