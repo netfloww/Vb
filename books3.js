2938435b424b9b89114cda30e8ce118b11f3f3e4
netfloww <wreosh3@gmail.com>
2026-09-11 02:45:03 +0300

    Create books3.js

---
+++ books3.js
@@ -0,0 +1,6 @@
+if (!window.booksData) window.booksData = {};
+window.booksData[3] = {
+    title: "صاحب الظل الطويل",
+    category: "روايات أدبية",
+    desc: "رواية رسائلية كلاسيكية تروي قصة الفتاة اليتيمة جودي أبوت ومحسنها الغامض."
+};
